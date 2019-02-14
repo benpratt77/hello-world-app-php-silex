dd07774cc38702cef633b087d0934bdae1f469d1
benpratt77 <ben.pratt@bigcommerce.com>
2019-02-14 15:32:08 +1100

    attempting to make v2 api requests

--- web/index.php
+++ web/index.php
@@ -3,7 +3,6 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Bigcommerce\Api\Client as Bigcommerce;
-use Firebase\JWT\JWT;
 use Guzzle\Http\Client;
 use Handlebars\Handlebars;
 use Silex\Application;
@@ -16,7 +15,7 @@ const BC_CLIENT_SECRET = 'gzaw2tnmcmuz5n1ewooo8ve7pv97ch1';
 const BC_CALLBACK_URL = 'https://sample-tax-app.herokuapp.com/auth/callback';
 const APP_URL = 'https://sample-tax-app.herokuapp.com/';
 
-//redis is not free with heroku
+//Redis is not free with heroku... this means that this doesn't work properly
 
 $app = new Application();
 $app['debug'] = true;
@@ -29,26 +28,24 @@ $app->get('/load', function (Request $request) use ($app) {
     }
     $headers = ['Access-Control-Allow-Origin' => '*'];
 
-    // Render the template with the recently purchased products fetched from the BigCommerce server.
-    $htmlContent = (new Handlebars())->render(file_get_contents(__DIR__ . '/../templates/details-form.html'), []);
-    $htmlContent = str_ireplace('http', 'https', $htmlContent); // Ensures we have HTTPS links, which for some reason we don't always get.
-    $response = new Response($htmlContent, 200, $headers);
-
-    /*** TESTING THE API **/
     $storeHash = 'u8stgwcn9s';
     configureBCApi($storeHash);
+    $bcClient = Bigcommerce::getConnection();
 
-    $userName = 'apples';
-    $password = 'carrots';
-    credentialise($userName, $password);
-    /*** !TESTING THE API **/
+    $currentConnection = $bcClient->get('https://api.bigcommerce.com/stores/u8stgwcn9s/v3/tax/connect/SampleTaxProvider', $data);
+    $message = "Welcome Back";
+
+    $htmlContent = (new Handlebars())->render(file_get_contents(__DIR__ . '/../templates/details-form.html'), []);
+    if ($currentConnection && $currentConnection->data && $currentConnection->data->configured) {
+        $htmlContent = (new Handlebars())->render(file_get_contents(__DIR__ . '/../templates/celebrate.html'), ['userName' => $currentConnection->data->username, 'message' => $message]);
+    }
+    $response = new Response($htmlContent, 200, $headers);
 
     return $response;
 });
 
 $app->get('/auth/callback', function (Request $request) use ($app) {
-
-    $payload = array(
+    $payload = [
         'client_id' => clientId(),
         'client_secret' => clientSecret(),
         'redirect_uri' => callbackUrl(),
@@ -56,12 +53,12 @@ $app->get('/auth/callback', function (Request $request) use ($app) {
         'code' => $request->get('code'),
         'scope' => $request->get('scope'),
         'context' => $request->get('context'),
-    );
+    ];
 
     $client = new Client(bcAuthService());
-    $req = $client->post('/oauth2/token', array(), $payload, array(
+    $req = $client->post('/oauth2/token', [], $payload, [
         'exceptions' => false,
-    ));
+    ]);
     $resp = $req->send();
 
     if ($resp->getStatusCode() == 200) {
@@ -79,132 +76,47 @@ $app->get('/remove-user', function (Request $request) use ($app) {
     if (empty($data)) {
         return 'Invalid signed_payload.';
     }
-
-//	$key = getUserKey($data['store_hash'], $data['user']['email']);
-//	$redis = new Credis_Client('localhost');
-//	$redis->del($key);
     return '[Remove User] ' . $data['user']['email'];
 });
 
-/**
- * GET /storefront/{storeHash}/customers/{jwtToken}/recently_purchased.html
- * Fetches the "Recently Purchased Products" HTML block and displays it in the frontend.
- */
-$app->get('/storefront/{storeHash}/customers/{jwtToken}/recently_purchased.html', function ($storeHash, $jwtToken) use ($app) {
+$app->post('/update-credentials', function (Request $request) use ($app) {
     $headers = ['Access-Control-Allow-Origin' => '*'];
-    try {
-        // First let's get the customer's ID from the token and confirm that they're who they say they are.
-        $customerId = getCustomerIdFromToken($jwtToken);
+    $data = [
+        'username' => $request->get('userName'),
+        'password' => $request->get('password')
+    ];
+    $storeHash = 'u8stgwcn9s';
+    configureBCApi($storeHash);
+    $bcClient = Bigcommerce::getConnection();
 
-        // Next let's initialize the BigCommerce API for the store requested so we can pull data from it.
-        configureBCApi($storeHash);
+    $bcClient->put('https://api.bigcommerce.com/stores/u8stgwcn9s/v3/tax/connect/SampleTaxProvider', $data);
+    $results = $bcClient->get('https://api.bigcommerce.com/stores/u8stgwcn9s/v3/tax/connect/SampleTaxProvider', $data);
 
-        // Generate the recently purchased products HTML
-        $recentlyPurchasedProductsHtml = getRecentlyPurchasedProductsHtml($storeHash, $customerId);
-
-        // Now respond with the generated HTML
-        $response = new Response($recentlyPurchasedProductsHtml, 200, $headers);
-    } catch (Exception $e) {
-        error_log("Error occurred while trying to get recently purchased items: {$e->getMessage()}");
-        $response = new Response("", 500, $headers); // Empty string here to make sure we don't display any errors in the storefront.
+    $message = 'Connection failed, probably due to v3 API-Proxy not being released yet. Try again later';
+    if ($results) {
+        $message = 'Congratulations you are now connected to BigCommerce. Great work';
     }
+
+    $htmlContent = (new Handlebars())->render(file_get_contents(__DIR__ . '/../templates/celebrate.html'), ['userName' => $request->get('userName'), 'message' => $message]);
+    $response = new Response($htmlContent, 200, $headers);
 
     return $response;
 });
 
+$app->post('/disconnect', function(){
+    $storeHash = 'u8stgwcn9s';
+    configureBCApi($storeHash);
+    $bcClient = Bigcommerce::getConnection();
+    $response =   $bcClient->delete('https://api.bigcommerce.com/stores/u8stgwcn9s/v3/tax/connect/SampleTaxProvider');
 
-$app->get('/enterCredentials', function ($storeHash) use ($app) {
-    $userName = 'apples';
-    $password = 'carrots';
-    credentialise($userName, $password);
+
+    $headers = ['Access-Control-Allow-Origin' => '*'];
+    $htmlContent = 'You have been susccessfully disconnected.';
+    $response = new Response($htmlContent, 200, $headers);
+
+    return $response;
+//    http_redirect('/load');
 });
-
-function credentialise($userName, $password)
-{
-    $client = new Client(bcAuthService());
-    $payload = [
-        'username' => $userName,
-        'password' => $password
-    ];
-    $headers = [
-        'client_id' => clientId(),
-        'auth_token' => getAuthToken('u8stgwcn9s'),
-        'store_hash' => 'u8stgwcn9s',
-        'access_token' => 'fq7dvv6by8tzwincwe1sjc041abmrpv',
-    ];
-
-    $headers = json_decode('{"access_token":"fq7dvv6by8tzwincwe1sjc041abmrpv","scope":"store_v2_default store_v2_information store_v2_products_read_only users_basic_information","user":{"id":1098843,"username":"ben.pratt@bigcommerce.com","email":"ben.pratt@bigcommerce.com"},"context":"stores\/u8stgwcn9s"}');
-
-//    $headers = [
-//        'access_token' => 'fq7dvv6by8tzwincwe1sjc041abmrpv',
-//        'client_id' => '68dp2ybd4w2vbae9pvlznv6hrzh2db2'
-//    ];
-    $url = 'https://api.bigcommerce.com/stores/u8stgwcn9s/v2/orders/';
-    $req = $client->get($url, $headers, ['exceptions' => false]);
-
-//    $url = 'https://api.bigcommerce.com/stores/u8stgwcn9s/v1/tax/connect/SampleTaxProvider';
-//    $req = $client->post($url, [], $payload, ['exceptions' => false]);
-
-
-    $resp = $req->send();
-
-    var_dump('Url: ' . $url . ' returned with code: ' . $resp->getStatusCode());
-}
-
-
-/**
- * Gets the HTML block that displays the recently purchased products for a store.
- * @param string $storeHash
- * @param string $customerId
- * @return string HTML content to display in the storefront
- */
-function getRecentlyPurchasedProductsHtml($storeHash, $customerId)
-{
-//	$redis = new Credis_Client('localhost');
-    $cacheKey = "stores/{$storeHash}/customers/{$customerId}/recently_purchased_products.html";
-    $cacheLifetime = 60 * 5; // Set a 5 minute cache lifetime for this HTML block.
-
-    // First let's see if we can find he HTML block in the cache so we don't have to reach out to BigCommerce's servers.
-    $cachedContent = json_decode($redis->get($cacheKey));
-    if (!empty($cachedContent) && (int)$cachedContent->expiresAt > time()) { // Ensure the cache has not expired as well.
-        return $cachedContent->content;
-    }
-
-    // Whelp looks like we couldn't find the HTML block in the cache, so we'll have to compile it ourselves.
-    // First let's get all the customer's recently purchased products.
-    $products = getRecentlyPurchasedProducts($customerId);
-
-    // Render the template with the recently purchased products fetched from the BigCommerce server.
-    $htmlContent = (new Handlebars())->render(
-        file_get_contents('templates/recently_purchased.html'),
-        ['products' => $products]
-    );
-    $htmlContent = str_ireplace('http', 'https', $htmlContent); // Ensures we have HTTPS links, which for some reason we don't always get.
-
-    // Save the HTML content in the cache so we don't have to reach out to BigCommece's server too often.
-//	$redis->set($cacheKey, json_encode([ 'content' => $htmlContent, 'expiresAt' => time() + $cacheLifetime]));
-
-    return $htmlContent;
-}
-
-/**
- * Look at each of the customer's orders, and each of their order products and then pull down each product resource
- * that was purchased.
- * @param string $customerId ID of the customer that we want to retrieve the recently purchased products list for.
- * @return array<Bigcommerce\Resources\Product> An array of products from the BigCommerce API
- */
-function getRecentlyPurchasedProducts($customerId)
-{
-    $products = [];
-
-    foreach (Bigcommerce::getOrders(['customer_id' => $customerId]) as $order) {
-        foreach (Bigcommerce::getOrderProducts($order->id) as $orderProduct) {
-            array_push($products, Bigcommerce::getProduct($orderProduct->product_id));
-        }
-    }
-
-    return $products;
-}
 
 /**
  * Configure the static BigCommerce API client with the authorized app's auth token, the client ID from the environment
@@ -226,18 +138,8 @@ function configureBCApi($storeHash)
  */
 function getAuthToken($storeHash)
 {
-    $authData = json_decode('{"access_token":"fq7dvv6by8tzwincwe1sjc041abmrpv","scope":"store_v2_default store_v2_information store_v2_products_read_only users_basic_information","user":{"id":1098843,"username":"ben.pratt@bigcommerce.com","email":"ben.pratt@bigcommerce.com"},"context":"stores\/u8stgwcn9s"}');
+    $authData = json_decode('{"access_token":"p1qorxexea81jyodql7u7o1ojf7ojnn","scope":"store_v2_default store_v2_information users_basic_information","user":{"id":1098843,"username":"ben.pratt@bigcommerce.com","email":"ben.pratt@bigcommerce.com"},"context":"stores\/u8stgwcn9s"}');
     return $authData->access_token;
-}
-
-/**
- * @param string $jwtToken customer's JWT token sent from the storefront.
- * @return string customer's ID decoded and verified
- */
-function getCustomerIdFromToken($jwtToken)
-{
-    $signedData = JWT::decode($jwtToken, clientSecret(), array('HS256', 'HS384', 'HS512', 'RS256'));
-    return $signedData->customer->id;
 }
 
 /**
@@ -304,10 +206,4 @@ function getUserKey($storeHash, $email)
     return "kitty.php:$storeHash:$email";
 }
 
-// redis set key kitty.php:u8stgwcn9s:ben.pratt@bigcommerce.com
-//store hash u8stgwcn9s
-
-
 $app->run();
-
-
